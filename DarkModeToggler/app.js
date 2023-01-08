51958a799e2e5cf799fbb66dbbdea2a076f3591a
SHOIB ANSARI <reddishshoib123@gmail.com>
2023-01-08 18:25:17 +0530

    comments added

--- DarkModeToggler/app.js
+++ DarkModeToggler/app.js
@@ -1,3 +1,4 @@
+//Function to change the theme mode
 function darkMode(){
      let setTheme = document.body
     setTheme.classList.toggle('dark_mode')
@@ -8,12 +9,15 @@ function darkMode(){
     }else {
         theme ='LIGHT'
     }
+     //save the theme into localStorage by jsonifying the theme into [ key ,value ] pair
     localStorage.setItem('pageItem', JSON.stringify(theme))
 
 }
 
+//get the theme of Website from localstorage
 let getTheme = JSON.parse(localStorage.getItem('pageItem'))
 
+//use the localStorage to set the mode when website opened again
 if (getTheme === 'DARK'){
     document.body.classList.add("dark_mode")
 }
